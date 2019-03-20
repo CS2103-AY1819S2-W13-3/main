81d1a3fa0c298bc75368ad3ba3fe9187f57a2199
Fabian <fabianterh@gmail.com>
2019-03-20 23:06:09 +0800

    Fix failing test: change test tag FOO to BAR

--- src/test/java/seedu/address/logic/commands/EditSourceDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditSourceDescriptorTest.java
@@ -2,12 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DETAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FOO;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TITLE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TYPE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 
 import org.junit.Test;
 
@@ -47,7 +42,7 @@ public class EditSourceDescriptorTest {
         assertFalse(DESC_AMY.equals(editedAmy));
 
         // different tags -> returns false
-        editedAmy = new EditSourceDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_FOO).build();
+        editedAmy = new EditSourceDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_BAR).build();
         assertFalse(DESC_AMY.equals(editedAmy));
     }
 }
